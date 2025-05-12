dcceec8441d3302d78639c3eea1f4465936d8420
Aanchal86 <theaanchal1225@gmail.com>
2025-05-12 16:30:24 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // all the code is treated as newer version of js
+
+// alert("hello") we are using nodejs, not browser
+/* number - 2^53
+bigint
+boolean - true/false
+string
+null - standalone value/ empty
+undefined - value not assigned
+symbol - to find uniqueness
+
+
+object - 
+ */
+let name = "aanchal"
+console.log(typeof(name));
+console.log(typeof null);
+console.log(typeof undefined)
